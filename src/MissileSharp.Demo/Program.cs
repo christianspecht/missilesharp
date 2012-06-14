8b2c6f365b4862f2cae6dcb5e9f8b7c69496c353
Christian Specht <christi@nspecht.de>
2012-06-14 16:44:03 +0200

    demo console application

---
+++ src/MissileSharp.Demo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MissileSharp;
+
+namespace MissileSharp.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press <RETURN> to run the demo!");
+            Console.ReadLine();
+
+            using (var launcher = new MissileLauncher(new ThunderMissileLauncher()))
+            {
+                launcher.Up(500);
+            }
+        }
+    }
+}
---
+++ src/MissileSharp.Demo/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MissileSharp.Example")]
+[assembly: AssemblyDescription("Demo application for MissileSharp")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("http://christianspecht.de/missilesharp/")]
+[assembly: AssemblyProduct("MissileSharp")]
+[assembly: AssemblyCopyright("© Christian Specht 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4bc1d765-007c-4e6e-b246-ab9e59222132")]
