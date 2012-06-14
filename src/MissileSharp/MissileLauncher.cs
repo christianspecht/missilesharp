eb0d5548db807c50543184d30aa6d274c24f46ab
Christian Specht <christi@nspecht.de>
2012-06-14 16:40:38 +0200

    minimal working example

--- src/MissileSharp/MissileLauncher.cs
+++ src/MissileSharp/MissileLauncher.cs
@@ -1,13 +1,58 @@
-﻿
+﻿using System;
+using System.Linq;
+using System.Threading;
+using HidLibrary;
+
 namespace MissileSharp
 {
-    public class MissileLauncher
+    public class MissileLauncher : IDisposable
     {
+        HidDevice device;
         ILauncherDevice launcher;
 
         public MissileLauncher(ILauncherDevice launcher)
         {
             this.launcher = launcher;
+
+            var devices = HidDevices.Enumerate(launcher.VendorId, launcher.DeviceId);
+            if (devices.Any())
+            {
+                device = devices.First();
+                device.OpenDevice();
+
+                while (!device.IsConnected || !device.IsOpen)
+                {
+                }
+            }
+        }
+
+        public bool IsReady
+        {
+            get
+            {
+                return (device != null && device.IsOpen);
+            }
+        }
+
+        public void Up(int milliseconds)
+        {
+            SendCommand(launcher.Up);
+            Thread.Sleep(milliseconds);
+            SendCommand(launcher.Stop);
+        }
+
+        public void Dispose()
+        {
+            if (IsReady)
+            {
+                device.CloseDevice();
+            }
+        }
+
+        private void SendCommand(byte command)
+        {
+            var data = launcher.CreateCommand(command);
+            device.Write(data);
         }
     }
 }
---
+++ src/MissileSharp/ThunderMissileLauncher.cs
@@ -0,0 +1,57 @@
+﻿
+namespace MissileSharp
+{
+    /// <summary>
+    /// settings for Dream Cheeky Thunder Missile Launcher
+    /// </summary>
+    public class ThunderMissileLauncher : ILauncherDevice
+    {
+        public int VendorId
+        {
+            get { return 0x2123; }
+        }
+
+        public int DeviceId
+        {
+            get { return 0x1010; }
+        }
+
+        public byte Down
+        {
+            get { return 1; }
+        }
+
+        public byte Up
+        {
+            get { return 2; }
+        }
+
+        public byte Left
+        {
+            get { return 4; }
+        }
+
+        public byte Right
+        {
+            get { return 8; }
+        }
+
+        public byte Stop
+        {
+            get { return 32; }
+        }
+
+        public byte Fire
+        {
+            get { return 16; }
+        }
+
+        public byte[] CreateCommand(byte command)
+        {
+            var data = new byte[9];
+            data[1] = 2;
+            data[2] = command;
+            return data;
+        }
+    }
+}
