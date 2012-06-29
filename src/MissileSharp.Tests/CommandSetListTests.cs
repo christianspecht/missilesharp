f01577a549f37616132fbb8f5b45a6a327a49180
Christian Specht <christi@nspecht.de>
2012-06-30 01:22:49 +0200

    new CommandSetList: wrapper for Dictionary<string, List<LauncherCommand>>

---
+++ src/MissileSharp.Tests/CommandSetListTests.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace MissileSharp.Tests
+{
+    [TestFixture]
+    public class CommandSetListTests
+    {
+        private CommandSetList list = null;
+
+        [SetUp]
+        public void Setup()
+        {
+            list = new CommandSetList();
+        }
+
+        [Test]
+        public void Add_CommandSetNameIsEmpty_ThrowsException()
+        {
+            Assert.Throws<InvalidOperationException>(() => list.Add(string.Empty, "cmd", 1));
+        }
+
+        [Test]
+        public void Add_CommandSetNameIsNull_ThrowsException()
+        {
+            Assert.Throws<InvalidOperationException>(() => list.Add(null, "cmd", 1));
+        }
+
+        [Test]
+        public void Add_CommandIsNull_ThrowsException()
+        {
+            Assert.Throws<InvalidOperationException>(() => list.Add("name", null));
+        }
+
+        [Test]
+        public void Add_CommandIsValid_ListContainsOneItem()
+        {
+            list.Add("name", "cmd", 1);
+            Assert.AreEqual(1, list.GetCommandSet("name").Count);
+        }
+        
+        [Test]
+        public void Add_CommandIsValid_SavedCommandIsCorrect()
+        {
+            list.Add("name", "cmd", 1);
+            Assert.AreEqual("cmd", list.GetCommandSet("name")[0].Command);
+        }
+        
+        [Test]
+        public void Add_CommandIsValid_SavedValueIsCorrect()
+        {
+            list.Add("name", "cmd", 1);
+            Assert.AreEqual(1, list.GetCommandSet("name")[0].Value);
+        }
+        
+        [Test]
+        public void Add_TwoValidCommands_ListContainsTwoItems()
+        {
+            list.Add("name", "cmd1", 1);
+            list.Add("name", "cmd2", 1);
+
+            Assert.AreEqual(2, list.GetCommandSet("name").Count);
+        }
+        
+        [Test]
+        public void CountSets_EmptyList_ReturnsZero()
+        {
+            Assert.AreEqual(0, list.CountSets());
+        }
+
+        [Test]
+        public void CountSets_ListWithOneItem_ReturnsOne()
+        {
+            list.Add("name", "cmd", 1);
+            Assert.AreEqual(1, list.CountSets());
+        }
+
+        [Test]
+        public void CountCommands_NonExistingCommandSet_ReturnsZero()
+        {
+            Assert.AreEqual(0, list.CountCommands("invalid"));
+        }
+
+        [Test]
+        public void CountCommands_ExistingCommandSet_ReturnsCorrectNumberOfItems()
+        {
+            list.Add("name","cmd",1);
+            Assert.AreEqual(1, list.CountCommands("name"));
+        }
+
+        [Test]
+        public void GetCommandSet_NonExistingCommandSet_ReturnsEmptyList()
+        {
+            Assert.AreEqual(0, list.GetCommandSet("invalid").Count);
+        }
+
+        [Test]
+        public void GetCommandSet_ExistingCommandSet_ReturnsListWithCorrectNumberOfItems()
+        {
+            list.Add("name", "cmd", 1);
+            Assert.AreEqual(1, list.GetCommandSet("name").Count);
+        }
+
+        [Test]
+        public void GetCommandSet_ExistingCommandSet_ReturnsValidCommand()
+        {
+            list.Add("name", "cmd", 1);
+            Assert.AreEqual("cmd", list.GetCommandSet("name")[0].Command);
+        }
+
+        [Test]
+        public void GetCommandSet_ExistingCommandSet_ReturnsValidValue()
+        {
+            list.Add("name", "cmd", 1);
+            Assert.AreEqual(1, list.GetCommandSet("name")[0].Value);
+        }
+    }
+}
---
+++ src/MissileSharp/CommandSetList.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MissileSharp
+{
+    /// <summary>
+    /// A dictionary of lists of LauncherCommands, searchable by command set name
+    /// </summary>
+    public class CommandSetList
+    {
+        private Dictionary<string, List<LauncherCommand>> dict = null;
+
+        /// <summary>
+        /// Creates a new instance of the LauncherCommandList class
+        /// </summary>
+        public CommandSetList()
+        {
+            dict = new Dictionary<string, List<LauncherCommand>>();
+        }
+
+        /// <summary>
+        /// Add a new item to a list of LauncherCommands (dictionary entry will be created if it doesn't exist)
+        /// </summary>
+        /// <param name="commandSetName">The name of the command set</param>
+        /// <param name="command">The actual command set (a list of commands)</param>
+        public void Add(string commandSetName, LauncherCommand command)
+        {
+            if (string.IsNullOrEmpty(commandSetName))
+            {
+                throw new InvalidOperationException("command set name is empty");
+            }
+
+            if (command == null)
+            {
+                throw new InvalidOperationException("command set is empty");
+            }
+
+            if (!dict.ContainsKey(commandSetName))
+            {
+                dict.Add(commandSetName, new List<LauncherCommand>());
+            }
+
+            dict[commandSetName].Add(command);
+        }
+
+        /// <summary>
+        /// Add a new item to a list of LauncherCommands (dictionary entry will be created if it doesn't exist)
+        /// </summary>
+        /// <param name="commandSetName">The name of the command set</param>
+        /// <param name="command">The actual command ("left", "fire" etc.)</param>
+        /// <param name="value">Numeric value for the command (duration/number of shots)</param>
+        public void Add(string commandSetName, string command, int value)
+        {
+            Add(commandSetName, new LauncherCommand(command, value));
+        }
+
+        /// <summary>
+        /// Get a command set (=list of LauncherCommands) by name
+        /// </summary>
+        /// <param name="commandSetName">The name to search for</param>
+        /// <returns>A list of command sets (empty if the name is not found)</returns>
+        public List<LauncherCommand> GetCommandSet(string commandSetName)
+        {
+            var list = new List<LauncherCommand>();
+
+            if (dict.ContainsKey(commandSetName))
+            {
+                list = dict[commandSetName];
+            }
+
+            return list;
+        }
+
+        /// <summary>
+        /// Counts the number of command sets.
+        /// </summary>
+        public int CountSets()
+        {
+            return dict.Count;
+        }
+
+        /// <summary>
+        /// Counts the number of commands in a specific command set.
+        /// </summary>
+        /// <param name="commandSetName">The name of the command set</param>
+        /// <returns></returns>
+        public int CountCommands(string commandSetName)
+        {
+            return GetCommandSet(commandSetName).Count;
+        }
+    }
+}
