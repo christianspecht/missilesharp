a5ff23fbe72ec32007457c41e056cca4e90c0348
Christian Specht <christi@nspecht.de>
2012-06-29 21:46:31 +0200

    helper class to read config files

---
+++ src/MissileSharp.Tests/ConfigReaderTests.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace MissileSharp.Tests
+{
+    [TestFixture]
+    public class ConfigReaderTests
+    {
+        private Dictionary<string, List<LauncherCommand>> dict;
+
+        [TearDown]
+        public void TearDown()
+        {
+            dict = null;
+        }
+
+        // this is not in [SetUp] because there will be more tests with other config files
+        public Dictionary<string, List<LauncherCommand>> GetValidConfig()
+        {
+            var conf = new List<string>();
+
+            conf.Add("[name1]");
+            conf.Add("up,10");
+            conf.Add(string.Empty);
+            conf.Add("[name2]");
+            conf.Add("right,20");
+            conf.Add("fire,2");
+
+            string[] configFile = conf.ToArray();
+            return ConfigReader.Read(configFile);
+        }
+
+        [Test]
+        public void Read_ValidConfig_CreatesTwoDictionaryEntries()
+        {
+            dict = GetValidConfig();
+            Assert.AreEqual(2, dict.Count);
+        }
+
+        [Test]
+        public void Read_ValidConfig_NamesInConfigExistInDictionary()
+        {
+            dict = GetValidConfig();
+            Assert.True(dict.ContainsKey("name1"));
+            Assert.True(dict.ContainsKey("name2"));
+        }
+
+        [Test]
+        public void Read_ValidConfig_NameNotInConfigDoesNotExistInDictionary()
+        {
+            dict = GetValidConfig();
+            Assert.False(dict.ContainsKey("invalid"));
+        }
+
+        [Test]
+        public void Read_ValidConfig_Name1HasOneCommand()
+        {
+            dict = GetValidConfig();
+            Assert.AreEqual(1, dict["name1"].Count);
+        }
+
+        [Test]
+        public void Read_ValidConfig_Name2HasTwoCommands()
+        {
+            dict = GetValidConfig();
+            Assert.AreEqual(2, dict["name2"].Count);
+        }
+
+        [Test]
+        public void Read_ValidConfig_Name1TheCommandIsCorrect()
+        {
+            dict = GetValidConfig();
+            Assert.AreEqual("up", dict["name1"][0].Command);
+        }
+
+        [Test]
+        public void Read_ValidConfig_Name1TheValueIsCorrect()
+        {
+            dict = GetValidConfig();
+            Assert.AreEqual(10, dict["name1"][0].Value);
+        }
+
+        [Test]
+        public void Read_ValidConfig_Name2TheFirstCommandIsCorrect()
+        {
+            dict = GetValidConfig();
+            Assert.AreEqual("right", dict["name2"][0].Command);
+        }
+
+        [Test]
+        public void Read_ValidConfig_Name2TheFirstValueIsCorrect()
+        {
+            dict = GetValidConfig();
+            Assert.AreEqual(20, dict["name2"][0].Value);
+        }
+
+        [Test]
+        public void Read_ValidConfig_Name2TheSecondCommandIsCorrect()
+        {
+            dict = GetValidConfig();
+            Assert.AreEqual("fire", dict["name2"][1].Command);
+        }
+
+        [Test]
+        public void Read_ValidConfig_Name2TheSecondValueIsCorrect()
+        {
+            dict = GetValidConfig();
+            Assert.AreEqual(2, dict["name2"][1].Value);
+        }
+    }
+}
---
+++ src/MissileSharp/ConfigReader.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace MissileSharp
+{
+    /// <summary>
+    /// Reads command sets from a config file
+    /// </summary>
+    public static class ConfigReader
+    {
+        /// <summary>
+        /// Reads a config file into a Dictionary of command sets
+        /// </summary>
+        /// <param name="configFile">The config file to load</param>
+        /// <returns>A Dictionary of command sets</returns>
+        public static Dictionary<string, List<LauncherCommand>> Read(string configFile)
+        {
+            var commands = new Dictionary<string, List<LauncherCommand>>();
+
+            if (File.Exists(configFile))
+            {
+                string[] lines = File.ReadAllLines(configFile);
+                commands = Read(lines);
+            }
+
+            return commands;
+        }
+
+        /// <summary>
+        /// Reads a config file into a Dictionary of command sets
+        /// </summary>
+        /// <param name="configFileLines">The config file lines</param>
+        /// <returns>A Dictionary of command sets</returns>
+        public static Dictionary<string, List<LauncherCommand>> Read(string[] configFileLines)
+        {
+            var commands = new Dictionary<string, List<LauncherCommand>>();
+
+            string key = "";
+
+            foreach (string line in configFileLines)
+            {
+                if (line.StartsWith("[") && line.EndsWith("]"))
+                {
+                    key = line.Substring(1, line.Length - 2).ToLower();
+                    commands.Add(key, new List<LauncherCommand>());
+                }
+                else if (line.Length > 0)
+                {
+                    var items = line.Split(',');
+
+                    if (items.Length == 2)
+                    {
+                        int value;
+
+                        if (int.TryParse(items[1], out value))
+                        {
+                            commands[key].Add(new LauncherCommand(items[0], value));
+                        }
+                    }
+                }
+            }
+            return commands;
+        }
+    }
+}
