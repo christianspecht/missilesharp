12dfbb80af5cc8e0aa6ed73196437bff5a7359bb
Christian Specht <christi@nspecht.de>
2012-08-01 16:52:22 +0200

    bind loaded command sets to listbox with buttons

--- src/MissileSharp.Launcher/App.xaml.cs
+++ src/MissileSharp.Launcher/App.xaml.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Windows;
+﻿using System.Windows;
+using MissileSharp.Launcher.ViewModels;
 
 namespace MissileSharp.Launcher
 {
@@ -12,5 +8,14 @@ namespace MissileSharp.Launcher
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var window = new MainWindow();
+            var viewmodel = new MainWindowViewModel();
+            window.DataContext = viewmodel;
+            window.Show();
+        }
     }
 }
---
+++ src/MissileSharp.Launcher/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace MissileSharp.Launcher.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private CommandCenter model;
+
+        public ObservableCollection<string> CommandSets { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainWindowViewModel()
+        {
+            model = new CommandCenter(new ThunderMissileLauncher());
+            model.LoadCommandSets("settings.txt");
+
+            this.CommandSets = new ObservableCollection<string>(model.GetLoadedCommandSetNames());
+        }
+    }
+}
