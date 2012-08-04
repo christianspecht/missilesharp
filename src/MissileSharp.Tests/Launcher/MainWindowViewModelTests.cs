d60bd8d82135bca8e05b3d5bee5abd694d104294
Christian Specht <christi@nspecht.de>
2012-08-04 02:00:13 +0200

    add first test for WPF launcher app

---
+++ src/MissileSharp.Tests/Launcher/MainWindowViewModelTests.cs
@@ -0,0 +1,31 @@
+﻿using MissileSharp.Launcher.ViewModels;
+using NUnit.Framework;
+
+namespace MissileSharp.Tests.Launcher
+{
+    [TestFixture]
+    public class MainWindowViewModelTests
+    {
+        private MainWindowViewModel viewmodel;
+        
+        [SetUp]
+        public void Setup()
+        {
+            var config = new string[]
+            {
+                "[name1]",
+                "up,5",
+                "[name2]",
+                "up,5"
+            };
+
+            viewmodel = new MainWindowViewModel(config);
+        }
+
+        [Test]
+        public void Constructor_ConfigWithTwoCommandSets_ObservableCollectionContainsTwoNames()
+        {
+            Assert.AreEqual(2, viewmodel.CommandSets.Count);
+        }
+    }
+}
