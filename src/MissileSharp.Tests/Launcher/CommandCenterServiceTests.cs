994c056d7e32ed2d86f033e5c43b3ce87ab6ca19
Christian Specht <christi@nspecht.de>
2012-11-08 23:55:24 +0100

    more CommandCenterService tests

--- src/MissileSharp.Tests/Launcher/CommandCenterServiceTests.cs
+++ src/MissileSharp.Tests/Launcher/CommandCenterServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using MissileSharp.Launcher.Services;
+﻿using System;
+using MissileSharp.Launcher.Services;
 using Moq;
 using NUnit.Framework;
 
@@ -19,11 +20,32 @@ namespace MissileSharp.Tests.Launcher
             this.commandcenterservice = new CommandCenterService(this.appconfigservice.Object);
         }
 
+        public ICommandCenterService SetupCommandCenterService()
+        {
+            return new CommandCenterService(this.appconfigservice.Object);
+        }
+
         [Test]
         public void GetCommandCenter_ValidLauncher_ReturnsCommandCenter()
         {
-            var commandCenter = this.commandcenterservice.GetCommandCenter();
+            var commandCenter = SetupCommandCenterService().GetCommandCenter();
             Assert.True(commandCenter is CommandCenter);
+        }
+
+        [Test]
+        public void GetCommandCenter_InvalidLauncher_Throws()
+        {
+            this.appconfigservice.Setup(stub => stub.LauncherName).Returns("MissileSharp.Tests.InvalidLauncher");
+            Assert.Catch<Exception>(() => SetupCommandCenterService().GetCommandCenter());
+        }
+
+        [Test]
+        public void GetCommandCenter_EmptyLauncher_Throws()
+        {
+            this.appconfigservice.Setup(stub => stub.LauncherName).Returns((string)null);
+            this.appconfigservice.Setup(stub => stub.LauncherAssembly).Returns((string)null);
+
+            Assert.Catch<Exception>(() => SetupCommandCenterService().GetCommandCenter());
         }
     }
 }
