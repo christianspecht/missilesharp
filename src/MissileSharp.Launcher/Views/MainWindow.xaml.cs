5366aff563030f1550441a84e85252aca193ad57
Christian Specht <christi@nspecht.de>
2012-11-20 01:37:11 +0100

    style launcher window with MahApps.Metro

--- src/MissileSharp.Launcher/Views/MainWindow.xaml.cs
+++ src/MissileSharp.Launcher/Views/MainWindow.xaml.cs
@@ -1,11 +1,11 @@
-﻿using System.Windows;
+﻿using MahApps.Metro.Controls;
 
 namespace MissileSharp.Launcher
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
