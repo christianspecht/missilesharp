cfa7783c645411b3cc6427aa162768560017d4dc
Christian Specht <christi@nspecht.de>
2012-12-26 20:00:33 +0100

    build ClickOnce setup in build script

--- build.bat
+++ build.bat
@@ -6,6 +6,6 @@ cd %~dp0
 
 call version-number.bat
 
-msbuild build.proj /p:CreateZip="%1" /p:CreatePackage="%2" /p:RunTests="%3"
+msbuild build.proj /p:CreateZip="%1" /p:CreatePackage="%2" /p:RunTests="%3" /p:CreateClickOnce="%4"
 
 pause
