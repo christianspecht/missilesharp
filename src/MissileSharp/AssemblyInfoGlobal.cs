541fb5ed96f6d7084d424c488b07a8c71fa050d3
Christian Specht <christi@nspecht.de>
2013-01-01 12:56:22 +0100

    update copyright info to 2013

--- src/MissileSharp/AssemblyInfoGlobal.cs
+++ src/MissileSharp/AssemblyInfoGlobal.cs
@@ -5,6 +5,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("http://christianspecht.de/missilesharp/")]
 [assembly: AssemblyProduct("MissileSharp")]
-[assembly: AssemblyCopyright("© Christian Specht 2012")]
+[assembly: AssemblyCopyright("© Christian Specht 2012-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
